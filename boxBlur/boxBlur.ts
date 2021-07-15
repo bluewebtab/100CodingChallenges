8dd0bad184ce9bd25b57134439f2cbe87acf264f
Ronny <ronny.icne@gmail.com>
2021-07-14 20:09:32 -0700

    update

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -2,16 +2,22 @@
 export function boxBlur(image: number[][]): number[][] {
     const res = [];
 
+    //for loop the length of the image array - 2
     for (let y = 0; y < image.length - 2; y++)
-{
+{   
+    console.log(image.length - 2)
     const line = []
+    //for loop the array inside the array length [7,4,0,1,5] 5 - 2 = 3
     for(let x = 0; x < image[y].length - 2; x++){
+        console.log(y)
         let sum = 0;
         let count = 0;
-        
-        for(let a = y; a < y + 3; a++){
-            for(let b = x; b < x + 3; b++){
-
+        //for loop the y + 3 = 3 or 4
+        for(let a = 0; a < y + 3; a++){
+            //for loop the x + 3 = 3 or 4
+          
+            for(let b = 0; b < x + 3; b++){
+              
                 sum += image[a][b]
                 count++;
             }
