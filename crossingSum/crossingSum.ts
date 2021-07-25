8ffda126ac7348dcfbbcd45390f1c37e504b8a03
Ronny Gutierrez <ronny.icne@gmail.com>
2021-07-25 15:03:02 -0700

    update

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,24 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
 
+    const columnTotal = matrix[a].reduce((a, b) => a + b)
+     
+    let rowTotal = 0;
+
+    for(let i = 0; i < matrix.length; i++){
+      rowTotal = rowTotal + (i !== a ? matrix[i][b] : 0)
+    }
+
+    console.log(rowTotal)
+
+    return columnTotal + rowTotal
 }
+
+
 
 console.log(crossingSum([[1, 1, 1, 1], 
     [2, 2, 2, 2], 
+    [3, 3, 3, 3],
     [3, 3, 3, 3]], 1, 3));
+    console.log(crossingSum([[1, 1, 1, 1], 
+        [2, 2, 2, 2], 
+        [3, 3, 3, 3]], 1, 3));
--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,17 @@
 export function depositProfit(deposit: number, rate: number, threshold: number): number {
+  let account = deposit
+  let years = 0;
 
+
+  //use while loop 
+  //because we don't know how many loops to take
+  while(threshold > account){
+      account += account * (rate /100)
+      years++
+     
+  }
+
+  return years
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170))
