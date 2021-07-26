98bce89bd15ca0b9807e8e1a93c160b4e88380fb
Ronny Gutierrez <ronny.icne@gmail.com>
2021-07-26 14:35:12 -0700

    update

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,26 @@
 export function electionsWinners(votes: number[], k: number): number {
+    let inTheRunning = 0;
+    //find largest value
+    const mostVotes = Math.max(...votes);
+    console.log(mostVotes)
+    //return largest to smallest
+    const sortedVotes = votes.sort((a, b) => b - a)
+    console.log(sortedVotes)
 
+
+    //returns one because we have a winner
+    if(sortedVotes[0] !== sortedVotes[1] && k === 0){
+        return 1;
+    }
+
+    votes.forEach((voteCount) => {
+        if(mostVotes - voteCount < k){
+            console.log(voteCount)
+            inTheRunning++;
+        }
+    })
+
+    return inTheRunning;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
