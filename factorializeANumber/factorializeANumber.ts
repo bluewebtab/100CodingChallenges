6d81e51e3d906faaa6a7cbdf13ec6b616bb79a0b
Ronny Gutierrez <ronny.icne@gmail.com>
2021-07-28 15:16:10 -0700

    factorial

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,14 @@
 export function factorializeANumber(num: number): number {
+    //if n === 1 then you have to return something (Ex: 1) for
+    //recursion to work
+    if(num === 1) return 1;
+
+    //this will cause a recursion
+    //where num = num - 1 going through the function
+    return num * factorializeANumber(num - 1)
+
 
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
