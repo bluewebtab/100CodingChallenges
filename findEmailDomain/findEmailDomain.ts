9c62a6a8823ad12da81ece714d0ec71a338f1db4
Ronny <ronny.icne@gmail.com>
2021-07-31 13:56:18 -0700

    update 7/31/21

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,12 @@
 export function findEmailDomain(address: string): string {
 
+    //method returns the index within the calling string object of the
+    //last occurrence of the specified value
+    const lastAtIndex = address.lastIndexOf('@')
+
+    //add one to not get the @ symbol
+    return address.slice(lastAtIndex + 1, address.length)
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,16 @@
 export function firstDigit(inputString: string): string {
+    let arrayNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let inputArr = inputString.split('')
 
+    for(let i = 0; i < inputArr.length; i++){
+        //The includes mehtod determine whether an array includes a certain value among its entries
+        //in an array
+        if(arrayNum.includes(inputArr[i])){
+            return inputString[i]
+        }
+    }
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));
