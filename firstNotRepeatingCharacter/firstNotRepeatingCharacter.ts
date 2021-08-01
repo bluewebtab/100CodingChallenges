867efd2f165290f84657211ff10267748647e612
Ronny Gutierrez <ronny.icne@gmail.com>
2021-08-01 16:29:23 -0700

    update

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,25 @@
 export function firstNotRepeatingCharacter(s: string): string {
+    let strArr = s.split('')
+    let visited = {}
 
+    for(let num of strArr){
+        if(visited[num]){
+            visited[num] += 1
+        }else{
+            visited[num] = 1
+        }
+       
+    }
+
+    for(let num in visited){
+        if(visited[num] == 1){
+            return num
+        }
+    }
+
+    return '_'
+    console.log(visited)
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter('abacabad'));
+console.log(firstNotRepeatingCharacter('abacabaabacaba'));
