f3a1655dbed84397e75628a8ef746f1471422b86
Ronny <ronny.icne@gmail.com>
2021-08-21 00:37:08 -0700

    update

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,16 @@
 export function newNumeralSystem(number: string): string[] {
+    const numerals: string[] = [];
+    let startCharCount = 65;
+    let endCharCount = number.charCodeAt(0);
+    console.log(startCharCount, endCharCount)
+    while(startCharCount <= endCharCount){
+        const numeral = `${String.fromCharCode(startCharCount)} + ${String.fromCharCode(endCharCount)}`;
+        numerals.push(numeral);
+        startCharCount++;
+        endCharCount--;
+    }
 
+    return numerals;
 }
 
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('G'));
