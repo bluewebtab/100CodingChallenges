b4a0257a0d48c9797e0667b67c24624d2b2fb2c0
Ronny <ronny.icne@gmail.com>
2021-08-28 17:07:30 -0700

    update

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,24 @@
 export function pigLatin(str: string): string {
+    const letters: string[] = str.split("");
+    const vowelRegex = /[aeiou]/;
 
+    if(vowelRegex.test(str[0])){
+        return `${str}way`
+    }
+
+    while(true){
+        if(!vowelRegex.test(letters[0])){
+            const firstConsonant = letters.splice(0, 1)[0]
+            letters.push(firstConsonant)
+        }else{
+            break;
+        }
+    }
+
+    str = letters.join('') + 'ay';
+
+    return str;
 }
 
-// console.log(pigLatin("glove"));
-// console.log(pigLatin("eight"));
+console.log(pigLatin("glove"));
+console.log(pigLatin("eight"));
--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,10 @@
 export function properNounCorrection(noun: string): string {
 
+    const firstChar = noun[0].toUpperCase();
+    const restOfWord = noun.slice(1, noun.length).toLowerCase();
+
+    return firstChar.concat(restOfWord)
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));
